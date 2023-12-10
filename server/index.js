3a616dd881288149f6ebada0aecfced3d0caa6f3
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2023-12-11 01:41:24 +0530

    add : mongoDB setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+//mongoose connection function
+
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log(`Database connected ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+connectDB();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
